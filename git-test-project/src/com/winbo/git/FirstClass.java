9e78194058ae80e4abaa3620d83b4a428fa1b238
luoyw <705809993@qq.com>
2020-09-16 10:09:09 +0800

    修改fistclass

--- git-test-project/src/com/winbo/git/FirstClass.java
+++ git-test-project/src/com/winbo/git/FirstClass.java
@@ -3,10 +3,12 @@ package com.winbo.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("gitgitgit");
+        //
     }
 
     public void test(){
         int i=0;
         i += 10;
+        System.out.println(i);
     }
 }
